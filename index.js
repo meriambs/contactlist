5502d4def39c2b0afc6a5c813f6e27427384d381
meriambs <bensalah.meriem0@gmail.com>
2022-05-22 23:19:08 +0100

    route and controllers

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+
 const ContactRouter = require("./routes/contact")
+
 const app = express()
 
 const port = 5000
+
 const connectddb = require('./config /connectdb')
 
 
 connectddb()
+
 app.use(express.json())
+
 app.use('/api/user',ContactRouter)
 
 
--- routes/contact.js
+++ routes/contact.js
@@ -1,56 +1,29 @@
+const { Route } = require('express');
 const express = require('express')
 
 const ContactRouter = express.Router()
-const Contact = require('../model/Contact');
-
+const { deletContact,GetById,UpdateContact,AddContact,GetContact}=require('../controllers/contact')
 
 //get route 
 
-ContactRouter.get('/',async(req,res)=>{
-try{
-const Contacts = await Contact.find()
-res.status(200).send({Contacts,msg:'list of ocntact'})
-}catch{
-res.status(500).send('could not get contacts')
-}
-})
+ContactRouter.get('/',GetContact)
 
 //delet routes 
 
-ContactRouter.delete('/:id',async (req,res)=>{
-    try{
-        const{id} = req.params
-        const delContact = await Contact.findByIdAndDelete(id)
-res.status(200).send('could delet it ')
-    }catch(err){
-res.status(500).send('could not delete')
-    }
-})
+ContactRouter.delete('/:id',deletContact)
 
 
 //post route 
 
-ContactRouter.post('/add',async(req,res)=>{
-    try{
- const NewContact= new Contact(req.body)
- await NewContact.save()
-res.status(200).send({NewContact,msg:"you did it the user is added "})
-    }catch(err){
-        res.status(500).send('could not add the contact')
-    }
-})
+ContactRouter.post('/add',AddContact)
 
 //put route 
 
 
-ContactRouter.put('/:id',async(req,res)=>{
-    try{
-        const {id}= req.params
-        const updatedUser = await Contact.findByIdAndUpdate(id,{$set:req.body})
-res.status(200).send('the user is updated ',updatedUser)
-    }catch(err){
-        res.status(500).send('u didnt do it  , no he is not  updated')
-    }
-})
+ContactRouter.put('/:id',UpdateContact)
 
+
+//and naw it's the get by id 
+
+ContactRouter.get('/:id',GetById)
 module.exports = ContactRouter
